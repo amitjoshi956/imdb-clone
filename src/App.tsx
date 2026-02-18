9186f3f0710a32218c079744bbe42f8416502bb0
Amit Joshi <amitjoshi956@gmail.com>
2026-02-18 23:06:51 +0530

    Implemented code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import AppLayout from "@components/AppLayout";
-import Home from "@pages/Home";
-import Movies from "@pages/Movies";
-import Watchlist from "@pages/Watchlist";
+
+const Home = lazy(() => import("@pages/Home"));
+const Movies = lazy(() => import("@pages/Movies"));
+const Watchlist = lazy(() => import("@pages/Watchlist"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center h-screen bg-gray-950 text-white">
+            Loading...
+          </div>
+        }
+      >
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
