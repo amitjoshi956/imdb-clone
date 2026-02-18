ae79bd38739491a9b8a2fbe545251e29cc60a50d
Amit Joshi <amitjoshi956@gmail.com>
2026-02-18 22:19:29 +0530

    Setting up AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
+import AppLayout from "./components/AppLayout";
 import Movies from "./pages/Movies";
 import Watchlist from "./pages/Watchlist";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Movies />} />
-        <Route path="/watchlist" element={<Watchlist />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Movies />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
